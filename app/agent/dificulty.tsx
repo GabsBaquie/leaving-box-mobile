20124efabcfe075f4481c22a412fda44b797ae91
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2025-01-29 11:43:39 +0100

    Ajout de la mise en page et des images à l'écran de difficulté ; ajustement des styles dans ParallaxScrollView

--- app/agent/dificulty.tsx
+++ app/agent/dificulty.tsx
@@ -1,34 +1,56 @@
 import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { useThemeColor } from "@/hooks/useThemeColor";
-
 import { Link } from "expo-router";
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
 export default function DificultyScreen() {
   return (
-    <ParallaxScrollView>
+    <ParallaxScrollView style={styles.container}>
       <Image
-        style={styles.pageLogo}
-        source={require("@/assets/images/homePageLogo.png")}
+        style={{ width: 100, height: 100, margin: "auto", marginTop: 50 }}
+        source={require("@/assets/images/bomb-logo.png")}
       />
+      <Image 
+        style={{margin: "auto"}}
+       source={require("@/assets/images/leavingbox.png")}
+       />
+
+      <View style={styles.linkContainer}>
+        <Link href={"/"} style={[styles.navStyle, { backgroundColor: "green" }]}>
+          Easy
+        </Link>
+
+        <Link href={"/"} style={[styles.navStyle, { backgroundColor: "orange" }]}>
+          Medium
+        </Link>
+
+        <Link href={"/"} style={[styles.navStyle, { backgroundColor: "red" }]}>
+
+          Hard
+        </Link>
+      </View>
+
       <Link href={"/"} style={styles.navStyle}>
-        Back
-      </Link>
+          Back
+        </Link>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  pageLogo: {
-    width: 100,
-    height: 100,
-
+  container: {
+    padding: 24,
   },
+
+  linkContainer: {
+    padding: 24,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+
   navStyle: {
-    color: "blue",
     fontSize: 20,
     fontWeight: "bold",
+    padding: 10,
   },
 });
